e36343f53129bc3bfae6da3420e789f24c49527b
Anastasia Taucci <112522011+AnastasiaTaucci@users.noreply.github.com>
2025-05-13 13:50:10 -0700

    add asyncstorage to display saved data until it is fetched from supabase

--- context/ResourcesContext.tsx
+++ context/ResourcesContext.tsx
@@ -2,6 +2,8 @@ import { SupabaseNewResource, useAddResource } from "@/hooks/useAddResource";
 import useDeleteResource from "@/hooks/useDeleteResource";
 import useGetResources from "@/hooks/useGetResources";
 import useUpdateResource from "@/hooks/useUpdateResource";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { sortRoutes } from "expo-router/build/sortRoutes";
 import { createContext, useContext, useEffect, useState } from "react";
 
 export type Resource = {
@@ -41,10 +43,37 @@ export const ResourceProvider: React.FC<{ children: React.ReactNode }> = ({ chil
         updateResourceMutation.mutate(updatedResource);
     };
 
+    // load saved data on initial load
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const jsonValue = await AsyncStorage.getItem("Resources");
+                const storageResources = jsonValue != null ? JSON.parse(jsonValue) : null;
+
+                if (storageResources && storageResources.length) {
+                    setResources(storageResources)
+                } 
+                
+            } catch (error) {
+                console.error("Failed to load cached resources", error);
+            }
+        };
+
+        fetchData();
+    }, [])
+
+    // Save fresh Supabase data and update state
     useEffect(() => {
         if (data && !isFetching) {
-            console.log("Fetched data: ", data);
+            // console.log("Fetched data: ", data);
             setResources(data as Resource[]);
+
+            try{
+                const jsonValue = JSON.stringify(data)
+                AsyncStorage.setItem("Resources", jsonValue)
+            } catch (error) {
+                console.error("Failed to cache resources", error)
+            }
         } 
         if (isFetching) {
             console.log("Fetching data...");
