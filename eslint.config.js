f0be441297ee88219dcf3854ab2d0aa9e1256c03
Anastasia Taucci <112522011+AnastasiaTaucci@users.noreply.github.com>
2025-05-19 01:17:52 -0700

    change eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
-
-module.exports = defineConfig([
-  expoConfig,
-  eslintPluginPrettierRecommended,
-  {
-    ignores: ["dist/*"],
+module.exports = {
+  extends: ["expo", "prettier"],
+  ignorePatterns: ["/dist/*"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/no-unresolved": "off",
+    semi: ["error", "always"], // enforces semicolons
+    quotes: ["error", "sinle"], // enforces double quotes
+    "comma-dangle": ["error", "always-multiline"], // enforces trailing commas
   },
-]);
+};
